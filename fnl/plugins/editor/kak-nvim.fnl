87cafc89e150ed583985e2643be78102adb73a1b
vivi <135711465+codingvivi@users.noreply.github.com>
2025-06-10 04:04:42 +0200

    Add disabled kak-nvim (not as nice as helix sorry)

---
+++ fnl/plugins/editor/kak-nvim.fnl
@@ -0,0 +1,11 @@
+{1 "kak.nvim"
+  :enabled false
+  :url "https://codeberg.org/mirge/kak.nvim.git"
+  :version  "^7"
+  :event :VeryLazy
+  :dependencies :folke/which-key.nvim
+  :opts {:full true 
+         :which_key_integration true
+         :experimental {:rebind_visual_aiAI false}}}
+
+ 
---
+++ lua/plugins/editor/kak-nvim.lua
@@ -0,0 +1,2 @@
+-- :fennel:1749391655
+return {"kak.nvim", url = "https://codeberg.org/mirge/kak.nvim.git", version = "^7", event = "VeryLazy", dependencies = "folke/which-key.nvim", opts = {full = true, which_key_integration = true, experimental = {rebind_visual_aiAI = false}}, enabled = false}

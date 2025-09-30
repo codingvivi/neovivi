5a64a3d402baa7a987dfd42e714252363f4e479a
vivi <135711465+codingvivi@users.noreply.github.com>
2025-09-30 14:41:19 +0200

    Flatten plugin folder

--- fnl/plugins/init.fnl
+++
@@ -1,8 +0,0 @@
-[{1 :udayvir-singh/tangerine.nvim
-  :rtpdirs [:ftplugin] 
-  :custom ["./config/nvim/lsp"] 
-  :hooks [:onsave :oninit]}   ;; so lazy manages Tangerine
- {:import :plugins.ui}
- {:import :plugins.editor}  
- {:import :plugins.tools}  
- {:import :plugins.langs}]  
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,4 +1,4 @@
--- :fennel:1758729837
+-- :fennel:1758733204
 local function _1_(_, opts)
   local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
   parser_config.org = {install_info = {files = {"src/parser.c", "src/scanner.c"}, revision = "64cfbc213f5a83da17632c95382a5a0a2f3357c1", url = "https://github.com/milisims/tree-sitter-org"}, filetype = "org"}
